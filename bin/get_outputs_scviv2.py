cec1d8d767cbb55f9614eeaf060883475fe8e851
Pierre Boyeau <pierre.boyeau@gmail.com>
2022-12-21 02:39:44 -0800

    store sample order for distance matrices

--- bin/get_outputs_scviv2.py
+++ bin/get_outputs_scviv2.py
@@ -1,5 +1,6 @@
 import scanpy as sc
 from utils import load_config, make_parents, wrap_kwargs
+import pandas as pd
 
 
 @wrap_kwargs
@@ -18,14 +19,22 @@ def get_outputs_scviv2(
     _adata = sc.read_h5ad(adata_in)
     # compute group-specific distance matrices
     cell_specific_dists = _adata.obsm["mrvi_local_sample_dists"]
+    group_key_to_dist_keys = {}
+
+    sample_ordering_key = _adata.uns["sample_order_key"]
+    sample_ordering = _adata.uns[sample_ordering_key]
     for group_key in groups:
         cell_groups = _adata.obs[group_key]
         group_to_keys = {}
         for group in cell_groups.unique():
             group_mask = cell_groups == group
             dists = cell_specific_dists[group_mask].mean(0)
+            dists = pd.DataFrame(dists, index=sample_ordering, columns=sample_ordering)
             group_to_keys[group] = dists
-        _adata.uns[f"mrvi_local_sample_dists_{group_key}"] = group_to_keys
+        uns_key = f"mrvi_local_sample_dists_{group_key}"
+        _adata.uns[uns_key] = group_to_keys
+        group_key_to_dist_keys[group_key] = uns_key
+    _adata.uns["group_key_to_dist_keys"] = group_key_to_dist_keys
 
     make_parents(adata_out)
     _adata.write(filename=adata_out)
