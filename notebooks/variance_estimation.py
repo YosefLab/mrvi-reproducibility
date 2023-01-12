09ef140903a10e81fb1adfcfd4b7e3991a997c1b
Justin Hong <justin.hong@columbia.edu>
2023-01-11 20:58:38 -0500

    working efficient montecarlo for base variance

---
+++ notebooks/variance_estimation.py
@@ -0,0 +1,35 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %%
+A = np.random.randn(10, 10) * 10
+std = np.random.randn(1, 10)
+
+# %%
+# Monte carlo simulation of l2 distances between gaussian samples
+u = np.random.randn(20000, 10) * std
+u2 = np.random.randn(20000, 10) * std
+z, z2 = np.einsum('jk,ij->ik', A, u), np.einsum('jk,ij->ik', A, u2)
+mc_dists = np.linalg.norm(z - z2, axis=1)
+
+# %%
+# Partial analytic computation with monte carlo
+sigma = np.diag(std.flatten()**2)
+eigval, _ = np.linalg.eig(2 * A.T @ sigma @ A)
+mc_samples = np.random.randn(20000, 10)
+mc_squared_l2 = np.sum((mc_samples ** 2) * eigval.reshape(1, -1), axis=1)
+mc_dists_partial = mc_squared_l2 ** 0.5
+
+# %%
+plt.hist(mc_dists, bins=100, label="Full Monte Carlo", alpha=0.5)
+plt.hist(mc_dists_partial, bins=100, label="Partial Monte Carlo", alpha=0.5)
+plt.legend()
+
+# %%
+print(f"F Mean: {np.mean(mc_dists)}")
+print(f"F Std: {np.std(mc_dists)}")
+print(f"P Mean: {np.mean(mc_dists_partial)}")
+print(f"P Std: {np.std(mc_dists_partial)}")
+
+# %%
