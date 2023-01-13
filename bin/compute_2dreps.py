628608b69403bdd588d860b558446b465c5b4fbc
Pierre Boyeau <pierre.boyeau@gmail.com>
2023-01-12 18:21:05 -0800

    add other low dim reps

--- bin/compute_2dreps.py
+++ bin/compute_2dreps.py
@@ -4,6 +4,15 @@ import pymde
 from utils import make_parents, wrap_kwargs
 
 
+mde_kwargs = dict(
+    embedding_dim=2,
+    constraint=pymde.Standardized(),
+    repulsive_fraction=0.7,
+    device="cuda",
+    n_neighbors=15,
+)
+
+
 @wrap_kwargs
 def compute_2dreps(
     *,
@@ -22,18 +31,35 @@ def compute_2dreps(
     adata = sc.read_h5ad(adata_in)
     latent_keys = adata.uns.get("latent_keys", None)
     make_parents(adata_out)
-    latent_2d_keys = []
+
     if latent_keys is None:
         adata.write_h5ad(adata_out)
+
+    latent_pca_keys = []
+    latent_tsne_keys = []
+    latent_mde_keys = []
     for latent_key in latent_keys:
         latent = adata.obsm[latent_key]
-        latent2d = TSNE(perplexity=30).fit_transform(latent)
-        latent_2d_key = f"{latent_key}_2d"
-        adata.obsm[latent_2d_key] = latent2d
-        latent_2d_keys.append(latent_2d_key)
-    adata.uns["latent_2d_keys"] = latent_2d_keys
-    adata.write_h5ad(adata_out)
 
+        latent_pca = pymde.pca(latent, embedding_dim=2).cpu().numpy()
+        latent_pca_key = f"{latent_key}_pca"
+        adata.obsm[latent_pca_key] = latent_pca
+        latent_pca_keys.append(latent_pca_key)
+
+        latent_tsne = TSNE(perplexity=30).fit_transform(latent)
+        latent_tsne_key = f"{latent_key}_tsne"
+        adata.obsm[latent_tsne_key] = latent_tsne
+        latent_tsne_keys.append(latent_tsne_key)
+
+        latent_mde = pymde.preserve_neighbors(latent, **mde_kwargs).embed().cpu().numpy()
+        latent_mde_key = f"{latent_key}_mde"
+        adata.obsm[latent_mde_key] = latent_mde
+        latent_mde_keys.append(latent_mde_key)
+    adata.uns["latent_pca_keys"] = latent_pca_keys
+    adata.uns["latent_tsne_keys"] = latent_tsne_keys
+    adata.uns["latent_mde_keys"] = latent_mde_keys
+
+    adata.write_h5ad(adata_out)
 
 if __name__ == "__main__":
     compute_2dreps()
