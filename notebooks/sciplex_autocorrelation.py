e803cb1bd4e5e25013fad214f188116ad214dbac
Justin Hong <justin.hong@columbia.edu>
2023-02-22 21:46:15 -0500

    update autocorr notebook to include local autocorrelation plots

--- notebooks/sciplex_autocorrelation.py
+++ notebooks/sciplex_autocorrelation.py
@@ -8,12 +8,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import scanpy as sc
+import jax
+import jax.numpy as jnp
 import faiss
-from tqdm import tqdm
 
 import scvi_v2
 import scvi
 import scanpy as sc
+import seaborn as sns
+import scipy
 
 # %%
 method_name = "scviv2"
@@ -54,7 +57,6 @@ def faiss_hnsw_nn(X: np.ndarray, k: int):
     """Gpu HNSW nearest neighbor search using faiss."""
     X = np.ascontiguousarray(X, dtype=np.float32)
     res = faiss.StandardGpuResources()
-    # index = faiss.IndexFlatL2(X.shape[1])
     M = 32
     index = faiss.IndexHNSWFlat(X.shape[1], M, faiss.METRIC_L2)
     gpu_index = faiss.index_cpu_to_gpu(res, 0, index)
@@ -65,44 +67,64 @@ def faiss_hnsw_nn(X: np.ndarray, k: int):
 
 
 # %%
-nn_indices, nn_dists = faiss_hnsw_nn(latent_u, k=16)
+nn_indices, nn_dists = faiss_hnsw_nn(latent_u, k=51)
 # %%
 # Remove self
 nn_indices = nn_indices[:, 1:]
 nn_dists = nn_dists[:, 1:]
 # %%
-# TODO: vmap this
-def compute_autocorrelation(features_df, nn_indices, nn_dists, variant="binary"):
+def compute_rank_autocorrelation(
+    features_df, nn_indices, nn_dists, variant="binary", return_local=False
+):
+    features_df = dists_from_vehicle_df
+    variant = "binary"
     N = features_df.shape[0]
     K = nn_indices.shape[1]
-    autocorrelations = np.zeros(features_df.shape[1])
-    for i in tqdm(range(features_df.shape[1])):
-        if variant == "binary":
-            W = np.zeros((N, N))
-            np.add.at(W, (np.repeat(np.arange(N), K), nn_indices.ravel()), 1)
-        elif variant == "gaussian_kernel":
-            W = np.full((N, N), float("inf"))
-            np.put(
-                W, (np.repeat(np.arange(N), K), nn_indices.ravel()), nn_dists.ravel()
-            )
-            W = np.exp(-((W / nn_dists[:, K // 3][:, None]) ** 2))
-            W = W / (np.sum(W, axis=1, keepdims=True) + 1e-6)
-        else:
-            raise ValueError(f"Unknown variant {variant}")
-        x = features_df.values[:, i : i + 1]
-        autocorrelation = (W * (x @ x.T)).sum()
-        autocorrelations[i] = autocorrelation
+
+    if variant == "binary":
+        W = np.zeros((N, N))
+        np.add.at(W, (np.repeat(np.arange(N), K), nn_indices.ravel()), 1)
+        W = W / (np.sum(W, axis=1, keepdims=True) + 1e-6)
+    elif variant == "gaussian_kernel":
+        W = np.full((N, N), float("inf"))
+        np.put(W, (np.repeat(np.arange(N), K), nn_indices.ravel()), nn_dists.ravel())
+        W = np.exp(-((W / nn_dists[:, K // 3][:, None]) ** 2))
+        W = W / (np.sum(W, axis=1, keepdims=True) + 1e-6)
+    else:
+        raise ValueError(f"Unknown variant {variant}")
+
+    feature_vals = jnp.array(features_df.values)
+    feature_vals_ranks = scipy.stats.rankdata(feature_vals, axis=1) / N
+    local_autocorrelations = jax.lax.map(
+        lambda x: (W * jnp.outer(x, x)).sum(axis=1)
+        - W.mean() * jnp.outer(x, x).sum(axis=1),
+        feature_vals_ranks.T,
+    )
+    if return_local:
+        return pd.DataFrame(
+            jax.device_get(local_autocorrelations),
+            index=features_df.columns,
+            columns=features_df.index,
+        )
+
+    autocorrelations = local_autocorrelations.sum(axis=1)
     return pd.Series(
-        autocorrelations, index=features_df.columns, name="autocorrelation"
+        jax.device_get(autocorrelations),
+        index=features_df.columns,
+        name="autocorrelation",
     )
 
 
 # %%
-autocorr_df = compute_autocorrelation(
+autocorr_df = compute_rank_autocorrelation(
     dists_from_vehicle_df, nn_indices, nn_dists, variant="binary"
 )
+local_autocorr_df = compute_rank_autocorrelation(
+    dists_from_vehicle_df, nn_indices, nn_dists, variant="binary", return_local=True
+)
+
 # %%
-# Get top two and bottom two autocorrelated products
+# Get top k and bottom k autocorrelated products
 top_k = autocorr_df.nlargest(5)
 bot_k = autocorr_df.nsmallest(5)
 print(top_k)
@@ -123,7 +145,20 @@ for prod, autocorr in zip(prods, autocorr_vals):
     sc.pl.embedding(adata, "U_mde", color=f"{prod}_dist_to_vehicle", show=False)
     plt.title(f"{prod} Distance to Vehicle, Autocorrelation: {autocorr}")
     plt.savefig(
-        os.path.join(base_dir_path, f"notebooks/figures/{prod}_dist_to_vehicle.png")
+        os.path.join(base_dir_path, f"notebooks/figures/{prod}_dist_to_vehicle.png"),
+        bbox_inches="tight",
     )
     plt.clf()
+
+    adata.obs[f"{prod}_local_rank_autocorr"] = local_autocorr_df.loc[prod]
+    sc.pl.embedding(adata, "U_mde", color=f"{prod}_local_rank_autocorr", show=False, vmin=0, vmax=1e-4)
+    plt.title(f"{prod} Local Autocorrelation, Autocorrelation: {autocorr}")
+    plt.savefig(
+        os.path.join(
+            base_dir_path, f"notebooks/figures/{prod}_local_rank_autocorr.png"
+        ),
+        bbox_inches="tight",
+    )
+    plt.clf()
+
 # %%
