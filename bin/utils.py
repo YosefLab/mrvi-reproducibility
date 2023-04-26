0dfc967ce572fdcb02cce2627a73f4ccab1fceb6
Pierre Boyeau <pierre.boyeau@gmail.com>
2023-04-25 19:22:01 -0700

    compartimentalize plot utils

--- bin/utils.py
+++ bin/utils.py
@@ -11,27 +11,8 @@ from typing import Callable
 import click
 import numpy as np
 import pandas as pd
-import plotnine as p9
 import scanpy as sc
 from remote_pdb import RemotePdb
-
-INCH_TO_CM = 1 / 2.54
-ALGO_RENAMER = {
-    "scviv2": "scviv2",
-    "scviv2_mlp": "scviv2(mlp)",
-    "scviv2_prior": "scviv2(prior)",
-    "scviv2_weighted": "scviv2(weighted)",
-    "scviv2_attention": "scviv2(attention)",
-    "composition_SCVI_clusterkey_subleiden1": "composition(SCVI)",
-    "composition_PCA_clusterkey_subleiden1": "composition(PCA)",
-}
-SHARED_THEME = p9.theme(
-    strip_background=p9.element_blank(),
-    subplots_adjust={"wspace": 0.3},
-    # panel_background=p9.element_blank(),
-    axis_text=p9.element_text(family="sans-serif", size=7),
-    axis_title=p9.element_text(family="sans-serif", size=8),
-)
 
 
 def make_parents(*paths) -> None:
